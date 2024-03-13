0a6ab240e2d72f8e91e84278b3f7cc553611fb5d
pavan-prasanth-guntrolu <119954621+pavan-prasanth-guntrolu@users.noreply.github.com>
2024-03-13 18:26:06 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+import "./index.css";
+
+import StarRating from "./StarRating.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+  </>
+);
